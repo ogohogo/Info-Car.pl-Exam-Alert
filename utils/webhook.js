e2760cb1a0eebf10bc36e834fb84da46287fec30
Bartosz Pijet <bartosz.pijet@essin.pl>
2023-01-17 11:26:58 +0100

    Implement notification via smsapi

--- utils/webhook.js
+++ utils/webhook.js
@@ -17,5 +17,21 @@ module.exports = {
     telegram: content => {
         const bot = new TelegramBot(config.telegram.botToken, { polling: false });
         bot.sendMessage(config.telegram.chatId, content);
-    }
+    },
+    smsapi: async content => {
+        await fetch('https://api.smsapi.pl/sms.do', {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${config.smsapi.apiToken}`,
+            },
+            "body": JSON.stringify({
+                "message": content,
+                "to": config.smsapi.phoneNumber,
+                "from": config.smsapi.fromNumber,
+                "format": "json",
+                "encoding": "utf-8",
+            })
+        })
+    },
 }
